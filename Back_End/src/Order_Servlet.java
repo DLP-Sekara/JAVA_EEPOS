dae8681d681a0f39469fa415f6e8264d3b45ebb3
DLP-Sekara <dslahirupiyumal99@gmail.com>
2022-05-18 17:32:10 +0530

    added item and order servlet

--- Back_End/src/Order_Servlet.java
+++ Back_End/src/Order_Servlet.java
@@ -4,12 +4,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
-@WebServlet(urlPatterns = "/customer")
-public class Customer_Servlet extends HttpServlet {
+@WebServlet(urlPatterns = "/order")
+public class Order_Servlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("customer done");
+        System.out.println("order done");
 
     }
 }
